affd2830eec6fdf1fde73de3087438157b6408d8
itayvallach <itay@run.ai>
2020-11-09 17:40:51 +0200

    wip

--- dags/kube.py
+++ dags/kube.py
@@ -16,14 +16,14 @@ default_args = {
 }
 
 dag = DAG(
-    'kubernetes_hello_world',
+    'runai_job',
     default_args=default_args,
     schedule_interval=timedelta(minutes=10))
 
 
 start = DummyOperator(task_id='start', dag=dag)
 
-passing = KubernetesPodOperator(namespace='default',
+job = KubernetesPodOperator(namespace='default',
                           image="python:3.6",
                           cmds=["python","-c"],
                           arguments=["print('hello world')"],
@@ -34,21 +34,6 @@ passing = KubernetesPodOperator(namespace='default',
                           dag=dag
                           )
 
-failing = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:16.04",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
-
-end = DummyOperator(task_id='end', dag=dag)
 
 
-passing.set_upstream(start)
-failing.set_upstream(start)
-passing.set_downstream(end)
-failing.set_downstream(end)
+job.set_upstream(start)
